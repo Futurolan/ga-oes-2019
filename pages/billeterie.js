f23ae1cb60418b1c9546efb73a5359b56564df2b
bmagic <baptiste.routhieau@gmail.com>
2018-09-07 14:31:37 +0200

    Add meta to billeterie

--- pages/billeterie.js
+++ pages/billeterie.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Layout from 'components/Layout'
 import '../styles/styles.scss'
 import TicketContent from '../components/TicketContent'
+import Meta from '../components/Meta'
+import config from '../lib/config'
 
 class BilleteriePage extends React.Component {
   render () {
     return (
       <Layout name='billeterie-page has-bg-star'>
+        <Meta title={config.tickets.title} description={config.tickets.description} />
         <section className='section'>
           <div className='container'>
             <TicketContent />
