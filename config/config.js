e2fd4c54e1db8b36cac101ac32ac40c24d121c18
bmagic <baptiste.routhieau@gmail.com>
2018-09-14 22:14:23 +0200

    Remove specific stuff from home page.

--- config/config.js
+++ config/config.js
@@ -2,6 +2,13 @@ let config = {
   title: 'Gamers Assembly : Halloween Edition 2018',
   description: 'Retrouvez la Gamers Assembly édition Halloween très prochainement',
   image: `${process.env.BASE_URL}/static/img/logo-orange-200x200.png`,
+  home: {
+    hero: {
+      title: 'Gamers Assembly',
+      subtitle: 'Halloween Edition 2018',
+      background: '/static/img/bg-orange-area.jpg'
+    }
+  },
   news: {
     title: 'Actualités',
     description: 'Retrouvez toutes les actualités de la Gamers Assembly Halloween Edition 2018'
