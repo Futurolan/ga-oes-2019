81fe295eba1cb55823ee1f0e640376f4db22278f
bmagic <baptiste.routhieau@gmail.com>
2018-10-02 11:30:46 +0200

    Update dependencies & fix issues with new packages.

--- components/TicketContent/index.js
+++ components/TicketContent/index.js
@@ -15,8 +15,8 @@ function TicketContent ({ data: { loading, error, node } }) {
   if (node.url && node.weezeventId) {
     return <div className='ga-ticket-content'>
       {node.minor && <div className='box content'>
+        <h2 className='title is-size-5'>Informations pour les mineurs</h2>
         <p>
-          <h2 className='title is-size-5'>Informations pour les mineurs</h2>
           Vous devez télécharger l'autorisation parentale, l'imprimer, la remplir et l'apporter le jour de votre venue avec votre billet.
         </p>
         <div className='has-text-centered'>
@@ -27,8 +27,8 @@ function TicketContent ({ data: { loading, error, node } }) {
         <WeezeventIframe id={node.weezeventId} url={node.url} />
       </div>
       {node.rules && <div className='box content'>
+        <h2 className='title is-size-5'>Règlement intérieur</h2>
         <p>
-          <h2 className='title is-size-5'>Règlement intérieur</h2>
           L'acquisition d'un ticket entraîne adhésion au règlement intérieur disponible ci-dessous.
         </p>
         <div className='has-text-centered'>
--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,6 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <meta charSet='utf-8' />
-          <title>Gamers Assembly</title>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
           <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
