95352c71ea9f381851e88fb02b392c446d49f71d
bmagic <baptiste@bmagic.fr>
2019-02-13 11:10:40 +0100

    Update package Fix issue with iframe

--- components/WeezeventIframe/index.js
+++ components/WeezeventIframe/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import './styles.scss'
+
 class WeezeventIframe extends React.Component {
   componentDidMount () {
     const script = document.createElement('script')
@@ -9,9 +11,10 @@ class WeezeventIframe extends React.Component {
   }
 
   render () {
-    return <div className='ga-weezevent-iframe' dangerouslySetInnerHTML={{ __html: `
-      <a title="Logiciel billetterie en ligne" href="https://www.weezevent.com/?c=sys_widget" class="weezevent-widget-integration" target="_blank" data-src="${this.props.url}" data-width="650" data-height="600" data-id="${this.props.id}" data-resize="1" data-width_auto="1" data-noscroll="0" data-nopb="0">Chargement de la billetterie Weezevent</a>
-      ` }} />
+    return <div className='ga-weezevent-iframe has-text-centered'><div dangerouslySetInnerHTML={{ __html: `
+      <a title="Logiciel billetterie en ligne" href="https://www.weezevent.com/?c=sys_widget" class="weezevent-widget-integration" target="_blank" data-src="${this.props.url}" data-width="650" data-height="1200" data-id="${this.props.id}" data-resize="1" data-width_auto="1" data-noscroll="0" data-nopb="0">Chargement de la billetterie Weezevent</a>
+      ` }} /><a target='_blank' href={this.props.url}>Ouvrir la billetterie dans un nouvel onglet</a>
+    </div>
   }
 }
 
