a99ad8b9022f0d9814ad9a359f0f5f177001f398
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2019-03-21 09:42:17 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ let config = {
   logo: `/static/img/logo-oes-130x100.png`,
   description: 'Retrouvez la Gamers Assembly Occitanie Edition très prochainement',
   metaImagePath: `/static/img/logo-oes-130x100.png`,
-  gaTrackingId: 'UA-0000000-0',
+  gaTrackingId: 'UA-128777022-6',
   mainPartner: {
     logo: '/static/img/logo-occitanie-238x106.png',
     url: 'https://www.laregion.fr/'
@@ -51,11 +51,11 @@ let config = {
     title: 'Billetterie',
     description: 'Achetez vos places pour la Gamers Assembly Occitanie Edition',
     link: '/billetterie',
-    pass: {
+    /* pass: {
       title: 'Pass "Manager/Accompagnateur"',
       text: 'Pour obtenir des pass "Manager/Accompagnateur", les équipes inscrites aux tournois peuvent réaliser une demande via le lien ci-dessous.',
       button: 'Obtenir un pass'
-    },
+    },*/
     minor: {
       title: 'Informations pour les mineurs',
       text: 'Vous devez télécharger l\'autorisation parentale, l\'imprimer, la remplir et l\'apporter le jour de votre venue avec votre billet.',
@@ -91,8 +91,8 @@ let config = {
     formUrl: 'https://goo.gl/forms/84pHQfSp50RcuUel2'
   },
   social: {
-    twitter: 'https://twitter.com/GamersAssembly',
-    facebook: 'https://www.facebook.com/GamersAssembly',
+    twitter: 'https://twitter.com/OccitanieEsport',
+    facebook: 'https://www.facebook.com/OccitanieEsports',
     twitch: 'https://www.flickr.com/photos/futurolan',
     youtube: 'https://twitch.tv/gamers_assembly',
     flickr: 'https://www.youtube.com/channel/UCbfhRIAsc4xdRACnDUwRfRw',
