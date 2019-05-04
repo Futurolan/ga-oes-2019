b5a696edfa7ff07504525ec06a77378f98c38f52
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2019-05-04 16:25:41 +0200

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -1,6 +1,6 @@
 const menu = [
   { type: 'config', id: 'news' },
-  { type: 'config', id: 'tickets' },  
+  { type: 'config', id: 'tickets' },
   { type: 'external', title: 'ESL ProLeague', link: 'https://pro.eslgaming.com/occitanie/' },
   { type: 'config', id: 'tournaments' },
   { type: 'config', id: 'info' },
