6d340cc54a7642407d8e7ccc7a7585f243eed461
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2019-05-10 11:34:26 +0200

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -3,7 +3,10 @@ const menu = [
   { type: 'config', id: 'tickets' },
   { type: 'external', title: 'ESL ProLeague', link: 'https://pro.eslgaming.com/occitanie/' },
   { type: 'config', id: 'tournaments' },
-  { type: 'config', id: 'info' },
+  { type: 'nolink', title: 'Infos',
+    children: [
+      { type: 'page', title: 'Région Occitanie', id: 3575, link: '/region' }  
+    ] },
   { type: 'config', id: 'partners' }
 ]
 
