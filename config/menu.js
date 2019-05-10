8bdf7fba35fa8d7294ecebfc949372eee334c1b1
bmagic <baptiste@bmagic.fr>
2019-05-10 13:34:07 +0200

    Fix Eslint issue on menu.js

--- config/menu.js
+++ config/menu.js
@@ -3,7 +3,8 @@ const menu = [
   { type: 'config', id: 'tickets' },
   { type: 'external', title: 'ESL ProLeague', link: 'https://pro.eslgaming.com/occitanie/' },
   { type: 'config', id: 'tournaments' },
-  { type: 'nolink', title: 'Infos',
+  { type: 'nolink',
+    title: 'Infos',
     children: [
       { type: 'page', title: 'Région Occitanie', id: 3575, link: '/region' }
     ] },
